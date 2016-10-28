4dc89545551590eae0189e08cef9bea57b79be6f
lijinchuan <403479851@qq.com>
2016-10-28 19:53:06 +0800

    日志站点

---
+++ LJC.Com.LogService/LJC.Com.LogViewWeb/App_Start/RouteConfig.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Routing;
+
+namespace LJC.Com.LogViewWeb
+{
+    public static class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+        }
+    }
+}
---
+++ LJC.Com.LogService/LJC.Com.LogViewWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LJC.Com.LogViewWeb.Global" Language="C#" %>
